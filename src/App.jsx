f76b54a52e53897cdca4643cce184a3b27152525
Akash Singh <akashsingh.dev1@gmail.com>
2025-11-27 08:22:15 +0530

    initial commit of the project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+function App() {
+  console.log(`the react version use by this project is : ${React.version}`);
+  return <h1>React Tic-Tac-Toe</h1>;
+}
+
+export default App;
