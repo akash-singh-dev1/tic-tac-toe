31770b98df8d585effd205dba119af2994dbe2e6
Akash Singh <akashsingh.dev1@gmail.com>
2025-12-06 21:03:54 +0530

    chore: add conditional GitHub Pages config for deployment.
    -- Added a mode-based `base` setting in vite.config.js so the project builds correctly for GitHub Pages (`/tic-tac-toe/`) while keeping normalbuilds unaffected.
    -- Updated npm scripts in package.json to support a smooth and consistent deployment workflow.
    -- make the GameBoard component responsive by using (wrap:nowrap) on game-board ol class.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // If mode is "gh-pages", use GitHub Pages base path, else use root "/"
+    base: mode === "gh-pages" ? "/tic-tac-toe/" : "/",
+  };
+});
